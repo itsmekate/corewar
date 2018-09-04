ba6325eeb94055e84f2e74da027b3c7238ae2854
Anastasia CHEPURNA <achepurn@e1r6p8.unit.ua>
2018-09-04 11:34:07 +0300

    mv start

---
+++ vm/vm.h
@@ -0,0 +1,14 @@
+#ifndef COREWAR_H
+# define CPREWAR_H
+
+# include "../libft/libft.h"
+
+typedef struct 		s_corewar
+{
+	int				visual_mode;
+	int				dump;
+	int				cycles_to_die;
+
+}					t_corewar;
+
+#endif
