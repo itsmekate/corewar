6d9482bd6ec7f51c22faf1aecb83c196d3bc8aad
AnaChepurna <ch.nastasie@gmail.com>
2018-09-30 18:31:33 +0300

    store fixed

--- vm/functions/store.c
+++ vm/functions/store.c
@@ -48,15 +48,17 @@ void			store(t_corewar *corewar, t_process *process)
 		return ;
 	// printf("reg %i\n", get_arg(1, process->position + move, corewar));
 	status = get_value(&arg[0], process, corewar, &move);
-	if (arg[1] == REG_CODE && arg[2] < REG_NUMBER)
+	if (arg[1] == REG_CODE)
 	{
+		//printf("REG_CODE\n");
 		if (get_value(&arg[1], process, corewar, &move) && status)
 			process->reg[arg[2]] = arg[0];
 	}
 	else if (arg[1] == IND_CODE)
 	{
+		//printf("IND_CODE\n");
 		// printf("%08x\n", arg[0]);
-		get_arg(2, process->position + move, corewar);
+		arg[2] = get_arg(2, process->position + move, corewar);
 		move += 2;
 		// printf("%hi\n", (short)arg[2]);
 		if (status)
