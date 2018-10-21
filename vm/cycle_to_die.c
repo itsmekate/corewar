e48b162808b36c20479a11e0790174d6effc27d2
AnaChepurna <ch.nastasie@gmail.com>
2018-10-21 19:41:00 +0300

    all

--- vm/cycle_to_die.c
+++ vm/cycle_to_die.c
@@ -1,36 +1,45 @@
 #include "vm.h"
 
-static int		process_lives(t_corewar *corewar)
-{
-	int 	res;
-	t_list	*lst;
-	t_process *process;
+// static int		process_lives(t_corewar *corewar, t_player *player)
+// {
+// 	int 	res;
+// 	t_list	*lst;
+// 	t_process *process;
 
-	res = 0;
-	lst = corewar->processes;
-	while(lst)
-	{
-		process = lst->content;
-		res += process->alive;
-		lst = lst->next;
-	}
-	return (res);
-}
+// 	//printf("lives = %i\n", player->lives);
+// 	//player->lives = 0;
+// 	res = 0;
+// 	lst = corewar->processes;
+// 	while(lst)
+// 	{
+// 		process = lst->content;
+// 		if (process->player == player)
+// 			res += process->alive;
+// 		lst = lst->next;
+// 	}
+// 	printf("%i\n", res);
+// 	return (res);
+// }
 
 static void 	players_lives(t_corewar *corewar)
 {
-	int			i;
+	//int res;
+	//int i;
 	static int	n = 0;
 
-	i = -1;
-	if (process_lives(corewar) >= NBR_LIVE)
-	{	
-		n = 0;
-		corewar->cycle_to_die -= CYCLE_DELTA;
-		log_cycle_to_die(corewar);
-		return ;
-	}
+	//i = -1;
 	n++;
+	//while (++i < corewar->players_num)
+	//{
+		//res = process_lives(corewar, corewar->players[i]);
+		if (corewar->lives_all >= NBR_LIVE)
+		{	
+			n = 0;
+			corewar->cycle_to_die -= CYCLE_DELTA;
+			log_cycle_to_die(corewar);
+			return ;
+		}
+	//}
 	if (n >= MAX_CHECKS)
 	{
 		corewar->cycle_to_die -= CYCLE_DELTA;
@@ -44,37 +53,34 @@ void			cycle_to_die(t_corewar *corewar)
 	t_list		*lst;
 	t_process	*pr;
 
-	//printf("cycle_to_die!! %i\n", corewar->cycle);
-	// print_map(corewar);
-	// printf("\n");
-	// sleep(1);
+
 	players_lives(corewar);
+	//printf("ok = %i; all = %i; ctd = %i\n", corewar->lives_ok, corewar->lives_all, corewar->cycle_to_die);
+	//corewar->lives_ok = 0;
+	corewar->lives_all = 0;
+	int i = -1;
+	while (++i < corewar->players_num)
+	{
+		corewar->players[i]->lives = 0;
+	}
 	lst = corewar->processes;
 	while (lst)
 	{
-		//printf("procc\n");
 		pr = lst->content;
 		if (!pr->alive)
 		{
 			//printf("kill\n");
 			kill_process(corewar, pr);
 			lst = corewar->processes;
-			//printf("fgfgfdg\n");
 		}
 		else
-		{
-			//pr->alive = 0;
 			lst = lst->next;
-		}
-		//printf("lol\n");
 	}
 	lst = corewar->processes;
-	//printf("fgd\n");
 	while (lst)
 	{
 		pr = lst->content;
 		pr->alive = 0;
 		lst = lst->next;
 	}
-	//printf("all good\n");
 }
